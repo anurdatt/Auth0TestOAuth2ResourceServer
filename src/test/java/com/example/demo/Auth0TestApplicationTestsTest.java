fb3cd20203e03d0169c778beb764c0deb7662d0f
Anuran Datta1 <anurdatt@in.ibm.com>
2023-02-20 01:45:09 +0530

    Auth0 Resource Server - initial

---
+++ src/test/java/com/example/demo/Auth0TestApplicationTestsTest.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+//import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Auth0TestApplicationTestsTest {
+
+	@Test
+	void testContextLoads() {
+		//fail("Not yet implemented");
+	}
+
+}
